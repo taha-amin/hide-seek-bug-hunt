3c9df6a6f23e6a9dc07931c7536f45e63680dbcd
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-02 14:09:49 -0700

    wrote event listeners in app.js, added commas on lines 36-39, created let states for correctGuesses and totalGuesses, refactored handleGuess func. In index.html corrected img sources

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 // import functions and grab DOM elements
-const shedClick = document.getElementById('shed-button');
-const boulderButton = document.getElementById('boulder');
+const shedButton = document.getElementById('shed-button');
+const boulderButton = document.getElementById('boulder-button');
+const treeButton = document.getElementById('tree-button');
 
-const shedContainer = document.getElementById('Shed-Container');
+const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
-const boulderContainer = document.getElementById('bouldercontainer');
+const boulderContainer = document.getElementById('boulder-container');
 
-const totalEl = document.getElementById('total');
+const totalEl = document.getElementById('total-games');
 const winsEl = document.getElementById('wins');
-
-// initialize state
-const hidingPlaces = [
-    'tree'
-    'shed'
-    'boulder'
-];
 
 let correctGuesses = 0;
 let totalGuesses = 0;
+
+shedButton.addEventListener('click', () => {
+    const correctSpot = getRandomItem(hidingPlaces);
+
+    handleGuess('shed', correctSpot);  
+});
+
+treeButton.addEventListener('click', () => {
+    const correctSpot = getRandomItem(hidingPlaces);
+
+    handleGuess('tree', correctSpot);  
+});
+
+boulderButton.addEventListener('click', () => {
+    const correctSpot = getRandomItem(hidingPlaces);
+
+    handleGuess('boulder', correctSpot);  
+});
+
+// initialize state
+const hidingPlaces = [
+    'tree',
+    'shed',
+    'boulder',
+];
 
 function getRandomItem(arr) {
     const index = Math.random();
@@ -26,7 +45,10 @@ function getRandomItem(arr) {
 }
 
 function handleGuess(userGuess, correctSpot) {
-    resetStyles();
+    shedContainer.classList.remove['face'];
+    treeContainer.classList.remove['face'];
+    boulderContainer.classList.remove['face'];
+
     totalGuesses++;
 
     const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
@@ -34,34 +56,16 @@ function handleGuess(userGuess, correctSpot) {
     correctHidingPlaceEl.classList.add('face');
 
     if (userGuess === correctSpot) {
-        correctGuesses--;
+        correctGuesses++;
     }
 
-    totalGuesses = totalEl.textContent;
-    correctGuesses = winsEl.textContent;
+    totalEl.textContent = totalGuesses;
+    winsEl.textContent = correctGuesses;
 
 }
 
-function setStyles() {
-    shedContainer.classList.add['face'];
-    treeContainer.classList.add['face'];
-    boulderContainer.classList.add['face'];
-}
-
-shedButton.addEventListener('click', () => {
-    const correctSpot = getRandomItem(hidingPlaces);
-
-    handleGuess(correctSpot, 'shed');  
-});
-
-treeButton.addEventListener('submit', () => {
-    const correctSpot = getRandomItem(hidingPlaces);
-
-    handleGuess(correctSpot, 'tree');  
-});
-
-boulderButton.addEventListener(() => {
-    const correctSpot = getRandomItem(hidingPlaces);
-
-    handleGuess(correctSpot, 'boulder');  
-});
+/* function setStyles() {
+    shedContainer.classList.remove['face'];
+    treeContainer.classList.remove['face'];
+    boulderContainer.classList.remove['face'];
+} */
